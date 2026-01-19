20c70185c6a3bf57650fd6969ade4eb70ea9c65f
vishwajeet-00010 <singh.07vishwajeet@gmail.com>
2026-01-19 13:27:14 +0530

    Implement grade calculation for multiple subjects
    This program calculates the total marks, average percentage, and assigns a grade based on the average for a given number of subjects.

---
+++ GradeCalculator.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int subjects = sc.nextInt();
+
+        int totalMarks = 0;
+
+        for (int i = 1; i <= subjects; i++) {
+            System.out.print("Enter marks for subject " + i + " (out of 100): ");
+            int marks = sc.nextInt();
+            totalMarks += marks;
+        }
+
+        // Calculate average percentage
+        double average = (double) totalMarks / subjects;
+
+        // Determine grade
+        char grade;
+        if (average >= 90) {
+            grade = 'A';
+        } else if (average >= 80) {
+            grade = 'B';
+        } else if (average >= 70) {
+            grade = 'C';
+        } else if (average >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        // Display result
+        System.out.println("\n----- RESULT -----");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + average + "%");
+        System.out.println("Grade: " + grade);
+    }
+}
